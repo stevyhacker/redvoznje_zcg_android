df30fe82b585f8c2bfc5ee419a8b348f573c27ac
Stevan Bogosavljević <stevan_nk@ymail.com>
2014-08-18 22:21:32 +0200

    Finished getter methods in MyDatabase

--- app/src/main/java/me/zeljeznica/redvoznje/MainActivity.java
+++ app/src/main/java/me/zeljeznica/redvoznje/MainActivity.java
@@ -1,7 +1,6 @@
 package me.zeljeznica.redvoznje;
 
 import android.app.Activity;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,82 +11,64 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity implements OnItemSelectedListener {
 
-	private Cursor cursor;
-	private ListView listview;
-	private ListView listview2;
-	private MyDatabase db;
+    private ListView trainListView;
+    private MyDatabase db;
+    private AdapterForTrainDepartures adapter;
+    private ArrayList<TrainItem> values;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		listview = (ListView) findViewById(R.id.listView1);
-		listview2 = (ListView) findViewById(R.id.listView2);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-		ArrayAdapter<CharSequence> spinner_adapter = ArrayAdapter
-				.createFromResource(this, R.array.gradovi,
-						android.R.layout.simple_spinner_item);
-		spinner_adapter
-				.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-		spinner.setAdapter(spinner_adapter);
-		spinner.setOnItemSelectedListener(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        trainListView = (ListView) findViewById(R.id.trainListView);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.gradovi, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(this);
 
-		db = new MyDatabase(this);
+        db = new MyDatabase(this);
+        values = new ArrayList<TrainItem>();
 
-	}
+        adapter = new AdapterForTrainDepartures(this,values);
+        //TODO Set new adapter for train list view
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		cursor.close();
-		db.close();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
 
-	@Override
-	public void onItemSelected(AdapterView<?> parent, View view, int position,
-			long id) {
-		switch (position) {
-		case 0:
-			cursor = db.getNiksic();
-			listAdapters();
-			break;
-		case 1:
-			cursor = db.getPodgorica();
-			listAdapters();
-			break;
-		case 2:
-			cursor = db.getBijeloPolje();
-			listAdapters();
-			break;
-		case 3:
-			cursor = db.getBar();
-			listAdapters();
-			break;
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+        switch (position) {
+            case 0:
+                cursor = db.getNiksic();
+                break;
+            case 1:
+                cursor = db.getPodgorica();
+                break;
+            case 2:
+                values = db.getBijeloPolje();
+                adapter.notifyDataSetChanged();
+                break;
+            case 3:
+                cursor = db.getBar();
+                break;
 
-		}
-	}
+        }
+    }
 
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
 
-	}
-
-	public void listAdapters() {
-		ListAdapter adapter = new SimpleCursorAdapter(this,
-				android.R.layout.activity_list_item, cursor,
-				new String[] {"polazakiz" },
-		new int[] { android.R.id.text1 ,android.R.id.text2});
-		listview.setAdapter(adapter);
-		
-		 ListAdapter adapter2 = new SimpleCursorAdapter(this,
-		 android.R.layout.activity_list_item, cursor,
-		 new String[] { "vrijemepolaska" },
-		 new int[] { android.R.id.text1 });
-		 listview2.setAdapter(adapter2);
-		
-	}
+    }
 
 }
--- app/src/main/java/me/zeljeznica/redvoznje/MyDatabase.java
+++ app/src/main/java/me/zeljeznica/redvoznje/MyDatabase.java
@@ -18,57 +18,130 @@ public class MyDatabase extends SQLiteAssetHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public Cursor getBar() {
+//    public Cursor getBar() {
+//
+//        SQLiteDatabase db = getReadableDatabase();
+//        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+//
+//        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};  //GRE�KA U BAZI TREBALO BI DA PI�E polazakza
+//
+//        String sqlTables = "bar";
+//
+//        qb.setTables(sqlTables);
+//        Cursor c = qb.query(db, sqlSelect, null, null,
+//                null, null, null);
+//
+//        c.moveToFirst();
+//        return c;
+//
+//    }
+//
+//    public Cursor getNiksic() {
+//        SQLiteDatabase db = getReadableDatabase();
+//        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+//
+//        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};
+//
+//        String sqlTables = "niksic";
+//
+//        qb.setTables(sqlTables);
+//        Cursor c = qb.query(db, sqlSelect, null, null,
+//                null, null, null);
+//        c.moveToFirst();
+//        return c;
+//
+//    }
+//
+//    public Cursor getPodgorica() {
+//
+//        SQLiteDatabase db = getReadableDatabase();
+//        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+//
+//
+//        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};
+//
+//        String sqlTables = "podgorica";
+//
+//        qb.setTables(sqlTables);
+//        Cursor c = qb.query(db, sqlSelect, null, null,
+//                null, null, null);
+//
+//        c.moveToFirst();
+//        return c;
+//    }
+
+    public ArrayList<TrainItem> getBar() {
+
+        ArrayList<TrainItem> values = new ArrayList<TrainItem>();
 
         SQLiteDatabase db = getReadableDatabase();
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
-        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};  //GRE�KA U BAZI TREBALO BI DA PI�E polazakza
-
+        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};
         String sqlTables = "bar";
 
         qb.setTables(sqlTables);
-        Cursor c = qb.query(db, sqlSelect, null, null,
+        Cursor cursor = qb.query(db, sqlSelect, null, null,
                 null, null, null);
-
-        c.moveToFirst();
-        return c;
-
+            if (cursor.moveToFirst()) {
+                while (cursor.isAfterLast() == false) {
+                    TrainItem item = new TrainItem(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+                    values.add(item);
+                    cursor.moveToNext();
+                }
+            }
+        cursor.close();
+        return values;
     }
 
-    public Cursor getNiksic() {
+    public ArrayList<TrainItem> getPodgorica() {
+
+        ArrayList<TrainItem> values = new ArrayList<TrainItem>();
+
         SQLiteDatabase db = getReadableDatabase();
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
         String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};
-
-        String sqlTables = "niksic";
-
-        qb.setTables(sqlTables);
-        Cursor c = qb.query(db, sqlSelect, null, null,
-                null, null, null);
-        c.moveToFirst();
-        return c;
-
-    }
-
-    public Cursor getPodgorica() {
-
-        SQLiteDatabase db = getReadableDatabase();
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-
-
-        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};
-
         String sqlTables = "podgorica";
 
         qb.setTables(sqlTables);
-        Cursor c = qb.query(db, sqlSelect, null, null,
+        Cursor cursor = qb.query(db, sqlSelect, null, null,
                 null, null, null);
-
-        c.moveToFirst();
-        return c;
+        if (cursor.moveToFirst()) {
+            while (cursor.isAfterLast() == false) {
+                TrainItem item = new TrainItem(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+                values.add(item);
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        return values;
     }
+
+    public ArrayList<TrainItem> getNiksic() {
+
+        ArrayList<TrainItem> values = new ArrayList<TrainItem>();
+
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        String[] sqlSelect = {"_id", "polazakiz", "vrijemepolaska"};
+        String sqlTables = "niksic";
+
+        qb.setTables(sqlTables);
+        Cursor cursor = qb.query(db, sqlSelect, null, null,
+                null, null, null);
+        if (cursor.moveToFirst()) {
+            while (cursor.isAfterLast() == false) {
+                TrainItem item = new TrainItem(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+                values.add(item);
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        return values;
+    }
+
 
     public ArrayList<TrainItem> getBijeloPolje() {
 
@@ -83,13 +156,13 @@ public class MyDatabase extends SQLiteAssetHelper {
         qb.setTables(sqlTables);
         Cursor cursor = qb.query(db, sqlSelect, null, null,
                 null, null, null);
-            if (cursor.moveToFirst()) {
-                while (cursor.isAfterLast() == false) {
-                    TrainItem item = new TrainItem(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
-                    values.add(item);
-                    cursor.moveToNext();
-                }
+        if (cursor.moveToFirst()) {
+            while (cursor.isAfterLast() == false) {
+                TrainItem item = new TrainItem(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+                values.add(item);
+                cursor.moveToNext();
             }
+        }
         cursor.close();
         return values;
     }
